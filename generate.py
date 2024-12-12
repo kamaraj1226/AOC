8e5ca72da47a7f335b76293dfa5a9ac66495203f
kamaraj1226 <kamaraj1226@gmail.com>
2024-12-12 09:06:28 +0530

    changed default argument for year in genereate.py and formatted all other files

--- generate.py
+++ generate.py
@@ -4,53 +4,65 @@ import argparse
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_heading(day, year):
     # https://adventofcode.com/2024/day/9
     url = "https://adventofcode.com/{year}/day/{day}"
 
     r = requests.get(url.format(year=year, day=day))
-    soup = BeautifulSoup(r.content, 'html.parser')
-    heading = soup.find('article').find('h2').get_text()
+    soup = BeautifulSoup(r.content, "html.parser")
+    heading = soup.find("article").find("h2").get_text()
 
-    heading = heading.split('---')[1].split(':')[-1].strip()
+    heading = heading.split("---")[1].split(":")[-1].strip()
     return heading
 
-def generate_file_name(day, year, heading:str):
-    month = '12'
-    day = '0'+day if len(day) == 1 else day
-    
-    base_file_name = day+month+year+'_' + '_'.join(heading.split())
-    python_name = base_file_name + '.py'
-    txt_name = base_file_name + '.txt'
+
+def generate_file_name(day, year, heading: str):
+    month = "12"
+    day = "0" + day if len(day) == 1 else day
+
+    base_file_name = day + month + year + "_" + "_".join(heading.split())
+    python_name = base_file_name + ".py"
+    txt_name = base_file_name + ".txt"
 
     return python_name, txt_name
 
+
 def create_files(py_name, txt_name):
     base_dir = os.path.dirname(sys.argv[0])
-    data_path = '/datas/'
-    
-    with open(base_dir+'/'+py_name, 'w'):
+    data_path = "/datas/"
+
+    with open(base_dir + "/" + py_name, "w"):
         ...
-    
-    with open(base_dir+data_path+txt_name, 'w'):
+
+    with open(base_dir + data_path + txt_name, "w"):
         ...
+
 
 def main(args):
 
     day = args.day
     year = args.year
     # heading = get_heading(day, year)
-    heading = 'Guard Gallivant'
+    heading = "Guard Gallivant"
     py_name, txt_name = generate_file_name(day, year, heading)
     create_files(py_name, txt_name)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-            prog='generate.py',
-            description='Generate required files'
-        )
-    parser.add_argument('-y', '--year', required=True, type=str,help='Type the AOC year.')
-    parser.add_argument('-d','--day', required=True, type=str, help='Type the AOC day.')
+        prog="generate.py", description="Generate required files"
+    )
+    parser.add_argument(
+        "-y",
+        "--year",
+        required=False,
+        type=str,
+        help="Type the AOC year.",
+        default="2024",
+    )
+    parser.add_argument(
+        "-d", "--day", required=True, type=str, help="Type the AOC day."
+    )
     args = parser.parse_args()
     main(args)
--- helper.py
+++ helper.py
@@ -1,17 +1,14 @@
 import os
 import sys
 
+
 def get_file_name(test=True):
 
     file_name = sys.argv[0]
     base_path = os.path.dirname(file_name)
-    file_name = os.path.basename(file_name).split('.')[0] + '.txt'
-    data_dir = '/datas/'
+    file_name = os.path.basename(file_name).split(".")[0] + ".txt"
+    data_dir = "/datas/"
     if test:
-        return base_path+data_dir+"test.txt"
-    
+        return base_path + data_dir + "test.txt"
+
     return base_path + data_dir + file_name
-
-    
-
-print(get_file_name(False))
